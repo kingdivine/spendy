3cfa1b29ec38ec2c5894f302c0d5e04eeb6f3e6c
kingdivine <divineanamekwe@gmail.com>
2019-12-10 22:22:33 +0000

    Add dropdown

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import "./App.css";
 import BarGraph from "././components/BarGraph";
+import Select from "react-select";
 
 const App = () => {
+  const options = [
+    { value: "map", label: "Where in the world do I spend?" },
+    { value: "days", label: "On What days do I spend the most?" },
+    { value: "stores", label: "Who is taking my money?" },
+    { value: "average", label: "How much am I actually spending?" }
+  ];
+
   return (
     <div className="App">
       <h1>Spendy</h1>
-      <BarGraph />
+      <div>
+        <Select options={options} />
+      </div>
+      <div>
+        <BarGraph />
+      </div>
     </div>
   );
 };
