5bc22705518658656acd25af30876f21c468a5b8
Divine Anamekwe <divineanamekwe@gmail.com>
2019-12-13 16:54:03 +0000

    Retireve map from Google Maps ApI

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
       <div>
         <Select options={options} />
       </div>
-      <div>
+      <div id="map">
         <Map />
       </div>
     </div>
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+
+const KEY = "API_KEY_HERE";
+
+const mapStyle = {
+  width: "100%",
+  height: "400px"
+};
 
 const Map = () => {
-  return <div>MAP GOES HERE</div>;
+  const ref = useRef();
+
+  const createMap = () => {
+    new window.google.maps.Map(ref.current, {
+      zoom: 2,
+      center: {
+        lat: 43.642567,
+        lng: -79.387054
+      }
+    });
+  };
+
+  useEffect(() => {
+    const mapScript = document.createElement("script");
+    mapScript.src = `https://maps.googleapis.com/maps/api/js?key=${KEY}`;
+    window.document.body.appendChild(mapScript);
+    mapScript.addEventListener("load", () => {
+      createMap();
+    });
+  }, []);
+
+  return <div ref={ref} id="map" style={mapStyle} />;
 };
 
 export default Map;
